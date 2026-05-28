63ae6c039fafe55ca246fd8db6bcdf38b1428bc6
Tringapps-Divyatharshni <divyatharshni.m@tringapps.com>
2026-05-28 08:32:44 +0530

    Added some logic

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ console.log("Dashboard Loaded");
 console.log("Temporary buggy code");
 console.log("Temporary buggy code");
 console.log("Temporary buggy code");
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Add login validation logic here
+});
